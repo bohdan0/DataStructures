ad5dea4e07a37de46315cdda5b91e93d37074f4d
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-11-25 11:17:29 -0800

    ResizingIntSet

--- LRUCache_aa/lib/p01_int_set.rb
+++ LRUCache_aa/lib/p01_int_set.rb
@@ -69,18 +69,26 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count == num_buckets
+
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    spot = num % num_buckets
+    @store[spot]
   end
 
   def num_buckets
@@ -88,5 +96,24 @@ class ResizingIntSet
   end
 
   def resize!
+    # if @count == num_buckets
+    #   temp_store = @store.deep_dup
+    #   new_set = ResizingIntSet.new(num_buckets * 2)
+    #   temp_store.each do |bucket|
+    #     bucket.each do |item|
+    #       new_set.insert(item)
+    #     end
+    #   end
+    # end
+
+    new_store = Array.new(@store.size * 2) { [] }
+    @store.each do |bucket|
+      bucket.each do |item|
+        spot = item % new_store.size
+        new_store[spot].push(item)
+      end
+    end
+
+    @store = new_store
   end
 end
