fc60efbff0c75ccd4022084d16c4f32f69928bcc
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-11-25 11:39:20 -0800

    adds String and Array hash

--- LRUCache_aa/lib/p02_hashing.rb
+++ LRUCache_aa/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    sum = 0.hash
+    self.each do |el|
+      sum += el.hash
+      sum = sum.hash
+    end
+    (sum.hash + self.length.hash).hash
   end
 end
 
 class String
   def hash
+    str_arr = self.split("")
+    sum = 0
+    str_arr.each_with_index do |el, i|
+      sum += (el.ord + i).hash
+      sum = sum.hash
+    end
+    sum.hash
   end
 end
 
