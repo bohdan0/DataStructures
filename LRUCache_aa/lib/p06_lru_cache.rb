ea9aabbb8222278609a53491c46684daf9aac1c5
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-11-25 21:18:21 -0800

    add LRUCache

--- LRUCache_aa/lib/p06_lru_cache.rb
+++ LRUCache_aa/lib/p06_lru_cache.rb
@@ -2,9 +2,8 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
-  attr_reader :count
   def initialize(max, prc)
-    @map = HashMap.new
+    @map = HashMap.new(max)
     @store = LinkedList.new
     @max = max
     @prc = prc
@@ -15,6 +14,13 @@ class LRUCache
   end
 
   def get(key)
+    if @map[key]
+      link = @map[key]
+      update_link!(link)
+      link.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -25,12 +31,22 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    link = @store.append(key, val)
+    @map[key] = link
+    eject! if count > @max
+    val
   end
 
   def update_link!(link)
     # suggested helper method; move a link to the end of the list
+    @store.remove(link.key)
+    @store.append(link.key, link.val)
   end
 
   def eject!
+    link_to_delete = @store.first
+    @store.remove(link_to_delete.key)
+    @map.delete(link_to_delete.key)
   end
 end
