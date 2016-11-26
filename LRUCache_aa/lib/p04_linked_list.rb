c0b7156548f3acde5bda3d1026dd5fd1e621340a
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-11-25 19:12:01 -0800

    refactor Linked list

--- LRUCache_aa/lib/p04_linked_list.rb
+++ LRUCache_aa/lib/p04_linked_list.rb
@@ -17,6 +17,7 @@ class Link
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @val = nil
     @next.prev = @prev if @next
     @prev.next = @next if @prev
   end
@@ -46,68 +47,53 @@ class LinkedList
     @head.nil? && @tail.nil?
   end
 
-  def get(key, curr_link = @head)
-    return nil if empty?
-    return curr_link.val if curr_link.key == key
-    return nil if curr_link == @tail
+  def get(key)
+    self.each { |link| return link.val if link.key == key }
 
-    get(key, curr_link.next)
+    nil
   end
 
   def include?(key)
-    each do |link|
-      return true if link.key == key
-    end
+    self.each { |link| return true if link.key == key }
+
     false
   end
 
   def append(key, val)
-    if include?(key)
-      update(key, val)
+    include?(key) ? update(key, val) : create_node(key, val)
+  end
+
+  def create_node(key, val)
+    node = Link.new(key, val)
+    set_up_node(node)
+  end
+
+  def set_up_node(node)
+    node.prev = @tail
+    @tail = node
+
+    if @head
+      node.prev.next = node
     else
-      node = Link.new(key, val)
-
-      node.prev = @tail
-      @tail = node
-      if @head
-        node.prev.next = node
-      else
-        @head = node
-      end
+      @head = node
     end
   end
 
-  def update(key, val, curr_link = @head)
-    return nil if empty?
-    if curr_link.key == key
-      curr_link.val = val
-    end
-
-    return nil if curr_link.next.nil?
-
-    update(key, val, curr_link.next)
+  def update(key, val)
+    each { |link| return link.val = val if link.key == key }
   end
 
-  def remove(key, curr_link = @head)
-    return nil if empty?
-
-    if curr_link.key == key
-      curr_link.next.prev = curr_link.prev unless curr_link.next.nil?
-      curr_link.prev.next = curr_link.next unless curr_link.prev.nil?
-      curr_link.val = nil
-    end
-
-    return nil if curr_link == @tail
-
-    remove(key, curr_link.next)
+  def remove(key)
+    each { |link| return link.remove if link.key == key }
   end
 
-  def each(&prc)
+  def each
     curr_link = @head
     until curr_link.nil?
-      prc.call(curr_link)
+      yield(curr_link)
       curr_link = curr_link.next
     end
+
     self
   end
 
