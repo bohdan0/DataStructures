07143899905f8a3babd867be1748fb9b55b16812
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-11-25 18:28:48 -0800

    refactor Set file

--- LRUCache_aa/lib/p01_int_set.rb
+++ LRUCache_aa/lib/p01_int_set.rb
@@ -6,7 +6,7 @@ class MaxIntSet
   end
 
   def insert(num)
-    raise 'Out of bounds' unless is_valid?(num)
+    raise 'Out of bounds' unless valid?(num)
     @store[num] = true
   end
 
@@ -20,11 +20,8 @@ class MaxIntSet
 
   private
 
-  def is_valid?(num)
-    num.between?(0, @max)
-  end
-
-  def validate!(num)
+  def valid?(num)
+    num.between?(0, @max - 1)
   end
 end
 
@@ -33,7 +30,6 @@ class IntSet
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { [] }
-    @num_buckets = num_buckets
   end
 
   def insert(num)
@@ -51,7 +47,7 @@ class IntSet
   private
 
   def [](num)
-    spot = num % @num_buckets
+    spot = num % num_buckets
     @store[spot]
   end
 
@@ -96,17 +92,7 @@ class ResizingIntSet
   end
 
   def resize!
-    # if @count == num_buckets
-    #   temp_store = @store.deep_dup
-    #   new_set = ResizingIntSet.new(num_buckets * 2)
-    #   temp_store.each do |bucket|
-    #     bucket.each do |item|
-    #       new_set.insert(item)
-    #     end
-    #   end
-    # end
-
-    new_store = Array.new(@store.size * 2) { [] }
+    new_store = Array.new(num_buckets * 2) { [] }
     @store.each do |bucket|
       bucket.each do |item|
         spot = item % new_store.size
